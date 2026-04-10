ca9f2bf755cb3ead9879820c1bc459b2b776305d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-04-10 16:40:13 +0530

    Fix: Add 'use client' directive and fix drei version compatibility

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import dynamic from 'next/dynamic'
 
 // Dynamically import 3D components
 const Gym3DCanvas = dynamic(() => import('@/components/Gym3DScene'), { ssr: false })
